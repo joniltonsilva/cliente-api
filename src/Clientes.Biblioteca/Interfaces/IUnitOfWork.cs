af40e52d3bf342b44cac84f55a5776d00263fb8e
Jonilton Leite <joniltonsilva@gmail.com>
2022-05-11 22:17:28 -0300

    Projeto de Biblioteca para Persistencia

---
+++ src/Clientes.Biblioteca/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clientes.Biblioteca.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken);
+
+        Task EndTransactionAsync(IDbContextTransaction transaction, CancellationToken cancellationToken);
+
+        Task RollbackTransactionAsync(IDbContextTransaction transaction, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Clientes.Biblioteca/Persistencia/ClienteDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Clientes.Biblioteca.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clientes.Biblioteca.Persistencia
+{
+    public class ClienteDbContext : DbContext, IUnitOfWork
+    {
+        public ClienteDbContext(DbContextOptions<ClienteDbContext> options)
+         : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public async Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken)
+        {
+            return await Database.BeginTransactionAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task EndTransactionAsync(IDbContextTransaction transaction, CancellationToken cancellationToken)
+        {
+            await transaction.CommitAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task RollbackTransactionAsync(IDbContextTransaction transaction, CancellationToken cancellationToken)
+        {
+            await transaction.RollbackAsync(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
